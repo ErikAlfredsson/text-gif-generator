773f42ff5bea287504b8f3e3f830aadffb7e32cd
Erik Alfredsson <erikalfredsson@ustwo.com>
2018-12-03 19:58:38 +0100

    Select random background image

--- text_gif_generator.js
+++ text_gif_generator.js
@@ -2,12 +2,13 @@ var exec = require('child_process').exec;
 
 /* VARIABLES */
 
-const SPEED = 15; // in ms
+const RANDOM = true;
+const SPEED = 13; // in ms
 const SIZE = 1000; // resolution in pixels
 const FONT_SIZE = SIZE * 0.9; // points NOT BEING USED RIGHT NOW. IF YOU WANT TO USE ADD -pointSize ${FONT_SIZE} WHEN CREATING CHARACTER IMAGE
-const BACKGROUND_COLOR = 'black'; // in text (haha), could probably be hex somehow
+const BACKGROUND_COLOR = 'pink'; // in text (haha), could probably be hex somehow
 const FONT_COLOR = 'lightgreen'; // in text (haha), could probably be hex somehow
-const STROKE_WIDTH = 5;
+const STROKE_WIDTH = 2;
 const STROKE_COLOR = 'white';
 
 /* CONSTANTS */
@@ -15,6 +16,9 @@ const STROKE_COLOR = 'white';
 const OUTPUT_DIR = 'output';
 const SPACE_IMAGE_NAME = `${OUTPUT_DIR}/blank.gif`;
 const OUTPUT_GIF_NAME = 'output';
+const BACKGROUND_DIR = 'backgrounds';
+const BACKGROUND_NAME = 'background1.jpg';
+const BACKGROUND_RESIZED_NAME = 'background_resized.gif';
 
 /* COMMAND LINE */
 
@@ -46,52 +50,75 @@ if (!inputString) {
 const words = inputString.split(' ');
 const names = [];
 const promises = [sh(`convert -size ${SIZE}x${SIZE} canvas:${BACKGROUND_COLOR} ${SPACE_IMAGE_NAME}`)];
+const resizedBackgroundName = `${OUTPUT_DIR}/${BACKGROUND_RESIZED_NAME}`;
 
-// SOME 'PADDING' BEFORE STARTING
-names.push(SPACE_IMAGE_NAME, SPACE_IMAGE_NAME);
+function createGIF(backgroundPath) {
+  // SOME 'PADDING' BEFORE STARTING
+  names.push(SPACE_IMAGE_NAME, SPACE_IMAGE_NAME);
 
-words.forEach((word, wordIndex) => {
-  const characters = word.split('');
+  words.forEach((word, wordIndex) => {
+    const characters = word.split('');
 
-  characters.forEach(async (c, index) => {
-    console.log('.');
+    characters.forEach(async (c, index) => {
+      console.log('.');
 
-    const name = `${OUTPUT_DIR}/${wordIndex}_${index}.gif`;
-    // label:${c.toUpperCase()}
-    // -annotate +0+${SIZE} '${c.toUpperCase()}'
-    promises.push(
-      sh(
-        `convert -size ${SIZE}x${SIZE} \
+      const name = `${OUTPUT_DIR}/${wordIndex}_${index}.gif`;
+
+      promises.push(
+        sh(
+          `convert -size ${SIZE}x${SIZE} \
         xc:${BACKGROUND_COLOR} \
         -font Trebuchet \
         -pointSize ${FONT_SIZE} \
-        -tile background.jpg \
+        -tile ${resizedBackgroundName} \
         -fill ${FONT_COLOR} \
         -stroke ${STROKE_COLOR} \
         -strokewidth ${STROKE_WIDTH} \
         -gravity center \
         -draw "text 0,0 '${c.toUpperCase()}'" \
         ${name}`
-      )
-    );
+        )
+      );
 
-    names.push(name, SPACE_IMAGE_NAME);
+      names.push(name, SPACE_IMAGE_NAME);
+    });
+
+    names.push(SPACE_IMAGE_NAME, SPACE_IMAGE_NAME);
   });
 
-  names.push(SPACE_IMAGE_NAME, SPACE_IMAGE_NAME);
-});
+  Promise.all(promises).then(async () => {
+    const imageNames = names.join(' ');
 
-Promise.all(promises).then(async () => {
-  const imageNames = names.join(' ');
+    console.log('\nCreating gif...');
+    // CREATE GIF
+    await sh(`convert -loop 0 -delay ${SPEED} ${imageNames} ${OUTPUT_DIR}/${OUTPUT_GIF_NAME}.gif`);
 
-  console.log('\nCreating gif...');
-  // CREATE GIF
-  await sh(`convert -loop 0 -delay ${SPEED} ${imageNames} ${OUTPUT_DIR}/${OUTPUT_GIF_NAME}.gif`);
+    console.log('\nCleaning up mess...');
+    // CLEANUP
+    await sh(`rm -rf ${imageNames}`);
+    await sh(`rm ${resizedBackgroundName}`);
+    console.log(`\nDone! Now grab the gif called ${OUTPUT_GIF_NAME}.gif in the ${OUTPUT_DIR} folder`);
+  });
+}
 
-  console.log('\nCleaning up mess...');
-  // CLEANUP
-  await sh(`rm -rf ${imageNames}`);
-  console.log(`\nDone! Now grab the gif called ${OUTPUT_GIF_NAME}.gif in the ${OUTPUT_DIR} folder`);
-});
+async function resizeBackground(backgroundPath) {
+  const { stdout } = await sh(`identify -ping -format '%w %h' ${backgroundPath || BACKGROUND_NAME}`);
+  const size = stdout.split(' ');
+  const width = size[0];
+  const height = size[1];
 
-// convert -loop 0 -delay 10 flux_0.png flux_30.png flux_60.png flux_90.png flux_120.png flux_150.png flux_180.png flux_210.png flux_240.png flux_270.png flux_300.png flux_330.png output.gif
+  await sh(`convert ${backgroundPath} -resize ${SIZE}x${SIZE} ${resizedBackgroundName}`);
+}
+
+async function selectBackground() {
+  const { stdout } = await sh('a=(backgrounds/*.jpg); echo ${a[$((RANDOM % ${#a[@]}))]}');
+  return stdout.replace(/\n/g, '');
+}
+
+if (RANDOM) {
+  selectBackground()
+    .then(backgroundPath => resizeBackground(backgroundPath))
+    .then(() => createGIF());
+} else {
+  resizeBackground().then(() => createGIF());
+}
